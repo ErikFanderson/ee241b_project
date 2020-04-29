e667852835ae41f5afabf0797d66652595a0ac0b
Erik Anderson <efa@eecs.berkeley.edu>
2020-04-29 15:05:51 -0700

    edited rtl w/ new SRAM emulation added

--- sourceme.sh
+++ sourceme.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash 
 
+export PRJPATH=$PWD
+
 # Set PYTHONPATH accordingly
 if [ -z "$PYTHONPATH" ]
 then
@@ -9,4 +11,9 @@ else
 fi
 
 # Toolbox 
-cd toolbox; source sourceme.sh; cd ..
+cd toolbox; source sourceme.sh; cd $PRJPATH 
+
+# ASAP7 stuff
+cd vlsi; source sourceme.sh; cd $PRJPATH
+
+export PDK_DIR="/home/ff/ee241/spring20-labs/asap7PDK_r1p5"
